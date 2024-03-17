40d1bd0d352a6011a94792ad0605666be2f1d116
prem001 <premkumarofficil@gmail.com>
2024-03-17 14:53:25 +0530

    Update settings.py

--- Project/settings.py
+++ Project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-52*&38&bk9**3r@fex_hhdjzweouwg1!-^e(y7o4bb(5^r_ho2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
